61eb216551b89e8a13035ed8f0d29ff7f0c25b86
unknown <bhanusankhyan77@gmail.com>
2026-08-03 03:06:09 +0530

    server configuration

--- main.py
+++ main.py
@@ -114,4 +114,4 @@ if __name__ == "__main__":
     import uvicorn
     print_banner()
     print("[System] Starting FastAPI application server...")
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
